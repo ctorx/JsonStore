ce4aa1a50fbabd4dd8754111f550ecc409c18544
Matthew Marksbury <mmarksbury@gmail.com>
2017-11-03 14:15:57 -0700

    Added async support

--- src/JsonStore.cs
+++ src/JsonStore.cs
@@ -4,7 +4,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading;
 using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace ctorx.JsonStore
 {
@@ -13,6 +16,7 @@ namespace ctorx.JsonStore
     /// </summary>
     public abstract class JsonStore<TStoreItem, TIdType> where TStoreItem : class
     {
+        SemaphoreSlim Mutex = new SemaphoreSlim(1);
         static object Locker = new object();
 
         readonly IHostingEnvironment HostingEnvironment;
@@ -52,6 +56,27 @@ namespace ctorx.JsonStore
         }
 
         /// <summary>
+        /// Gets a set of items in the store
+        /// </summary>
+        public async Task<IQueryable<TStoreItem>> GetSetAsync()
+        {
+            using (var sourceStream = new FileStream(this.FilePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true))
+            {
+                var sb = new StringBuilder();
+
+                byte[] buffer = new byte[0x1000];
+                int numRead;
+                while ((numRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    var text = Encoding.Unicode.GetString(buffer, 0, numRead);
+                    sb.Append(text);
+                }
+
+                return JsonConvert.DeserializeObject<IList<TStoreItem>>(sb.ToString(), this.JsonStoreOptions.SerializerSettings).AsQueryable();
+            }
+        }
+
+        /// <summary>
         /// Saves an item to the store
         /// </summary>
         public void Save(TStoreItem item)
@@ -69,6 +94,26 @@ namespace ctorx.JsonStore
                 itemList[itemList.IndexOf(existing)] = item;
 
             this.WriteList(itemList);
+        }
+
+        /// <summary>
+        /// Saves an item to the store
+        /// </summary>
+        public async Task SaveAsync(TStoreItem item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            var itemList = (await this.GetSetAsync()).ToList();
+
+            var existing = itemList.FirstOrDefault(x => this.IdExpression(x).Equals(this.IdExpression(item)));
+
+            if (existing == null)
+                itemList.Add(item);
+            else
+                itemList[itemList.IndexOf(existing)] = item;
+
+            await this.WriteListAsync(itemList);
         }
 
         /// <summary>
@@ -90,7 +135,27 @@ namespace ctorx.JsonStore
 
             this.WriteList(itemList);
         }
-        
+
+        /// <summary>
+        /// Deletes an item from the store
+        /// </summary>
+        public async Task DeleteAsync(TStoreItem item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            var itemList = (await this.GetSetAsync()).ToList();
+
+            var existing = itemList.FirstOrDefault(x => this.IdExpression(x).Equals(this.IdExpression(item)));
+
+            if (existing == null)
+                throw new InvalidOperationException("item does not exist");
+
+            itemList.Remove(existing);
+
+            await this.WriteListAsync(itemList);
+        }
+
         /// <summary>
         /// Writes the list to disk
         /// </summary>
@@ -105,6 +170,31 @@ namespace ctorx.JsonStore
             {                 
                 File.WriteAllText(this.FilePath, serialized);
             }
+        }
+
+        /// <summary>
+        /// Writes the list to disk
+        /// </summary>
+        async Task WriteListAsync(IList<TStoreItem> list)
+        {
+            if (list == null)
+                list = new List<TStoreItem>();
+
+            var serialized = JsonConvert.SerializeObject(list, this.JsonStoreOptions.SerializerSettings);
+
+            await Mutex.WaitAsync();
+            try
+            {
+                var encodedText = Encoding.Unicode.GetBytes(serialized);
+                using (FileStream sourceStream = new FileStream(this.FilePath, FileMode.Append, FileAccess.Write, FileShare.None, 4096, true))
+                {
+                    await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
+                }
+            }
+            finally
+            {
+                this.Mutex.Release();
+            }            
         }
     }
 }
